01a32978cf1ed9362177b786cd71b8f1179ef2ca
alexxalexx <92640944+alexxalexx@users.noreply.github.com>
2022-04-02 14:23:29 +0300

    Update 14:23

--- 0204.js
+++ 0204.js
@@ -1,9 +1,9 @@
 let arr = [0,2,4,3,5];
 
+let squareNumber = 0;
+
 for(i = 0; i < arr.length; i++)
-{
-    let squareNumber = 0;
-    
+{ 
     squareNumber =  Math.pow(arr[i], 2);
 
     alert(squareNumber);
